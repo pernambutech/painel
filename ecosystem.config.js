ee0952d0b74f475f9cb66bd46e21de6f39c4b51b
pernambutech <pernambutech@gmail.com>
2026-08-28 17:49:45 -0300

    corrige inicialização do painel web no PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -40,7 +40,8 @@ module.exports = {
     {
       name: 'painel-web',
       cwd: path.join(raiz, 'apps', 'painel-web'),
-      script: path.join(raiz, 'apps', 'painel-web', '.next', 'standalone', 'server.js'),
+      script: path.join(raiz, 'node_modules', 'next', 'dist', 'bin', 'next'),
+      args: 'start -p 3000',
       interpreter: 'node',
       env: {
         NODE_ENV: 'production',
